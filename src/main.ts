c7cc2d15ef2de47fbc582942c54ed4beff7cbc30
Stephen Oney <swloney@gmail.com>
2018-02-12 00:18:42 -0500

    removed old code

--- src/main.ts
+++ src/main.ts
@@ -112,24 +112,4 @@ const GHDiscussionTemplate:string = 'GithubDiscussionTemplate';
         Reward: '0.01',
         MaxAssignments: 4
     });
-    // await mturk.createHITFromFile(join(__dirname, '..', 'templates', 'AmazonExample.xml'), {
-    //     Title: 'Test HIT over 5000',
-    //     Description: 'Testing the HIT API',
-    //     LifetimeInSeconds: 600,
-    //     AssignmentDurationInSeconds: 600,
-    //     Reward: '0.01',
-    //     MaxAssignments: 4
-    // });
-    // const hits = await mturk.listHITs({MaxResults: 10});
-    // hits.forEach(async (h) => {
-    //     // console.log(h);
-    //     const assignments = await h.listAssignments();
-    //     assignments.forEach((a, i) => {
-    //             a.getAnswers().forEach((v, k) => {
-    //                 console.log(k);
-    //                 console.log(v);
-    //             });
-    //         // console.log(h.getTitle());
-    //     });
-    // });
 })();
