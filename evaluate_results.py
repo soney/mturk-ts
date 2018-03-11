bdb702ca4bc2c301ded876f3302f01396c212a4a
Xu <sxu72013@gmail.com>
2018-03-11 13:01:59 -0400

    add status for each HIT

--- evaluate_results.py
+++ evaluate_results.py
@@ -1,7 +1,7 @@
 import json
 
 results = {}
-completed = {}
+HITS_status = {}
 with open('result.json') as f:
     data = json.load(f)
 
@@ -18,15 +18,20 @@ for issue_id in results:
         comment_agree = {}
         num_responders = len(results[issue_id][comment_id])
         for response in results[issue_id][comment_id]:
-            #num_responders = len(response)
             for ans in response:
                 if ans not in comment_agree:
                     comment_agree[ans] = 0
                 comment_agree[ans] += 1
         if max(comment_agree.values()) > 1:
             issue_agree[comment_id] = comment_agree
+    HITS_status[issue_id] = {}
+    print('len issue_agree {}'.format(len(issue_agree)))
+    print('len results[issue_id] {}'.format(len(results[issue_id])))
     if len(issue_agree) == len(results[issue_id]):
-        completed[issue_id] = issue_agree
-        completed[issue_id]["num_responders"] = num_responders
-with open('completed.json', 'w') as f:
-    f.write(json.dumps(completed))
+        HITS_status[issue_id]['status'] = 'completed'
+        HITS_status[issue_id]['agree_comments'] = issue_agree
+        HITS_status[issue_id]['num_responders'] = num_responders
+    else:
+        HITS_status[issue_id]['status'] = 'pending'
+with open('HITS_status.json', 'w') as f:
+    f.write(json.dumps(HITS_status))
