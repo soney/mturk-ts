bf18cb3f0f79508ff66c4054294b47c00ad3035f
Xu <sxu72013@gmail.com>
2018-04-08 12:47:00 -0400

    update code, change to prod param

---
+++ bin/production_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+
+counter=1
+while [ $counter -le 1 ]
+do
+  ((counter++)) >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  current_date_time="`date +%Y_%m_%d_%H_%M_%S`"
+  echo $current_date_time >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  npm start 2 >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "finished retrieving" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  python3 mturk_util.py 2 e >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "finished evaluating" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  python3 mturk_util.py 2 a >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "finished approving" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  python3 mturk_util.py 2 r >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "finished rejecting" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  npm start 1 >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "finished posting" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  echo "====================================================================================" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+  echo "====================================================================================" >> /Users/Xu/Desktop/gh_workspace/mturk/mturk-ts/log.txt
+
+  sleep 1800
+done
--- bin/test_run.sh
+++ bin/test_run.sh
@@ -4,9 +4,17 @@ npm start 2
 
 echo "finished retrieving"
 
-python3 mturk_util.py 1 e
+python3 mturk_util.py 2 e
 
 echo "finished evaluating"
+
+python3 mturk_util.py 2 a
+
+echo "finished approving"
+
+python3 mturk_util.py 2 r
+
+echo "finished rejecting"
 
 npm start 1
 
